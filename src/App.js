38bdefbe8f1e5f3403326817cda2204718166996
ayushi <aayushid81@gmail.com>
2025-01-20 20:38:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import AppRoutes from './routes/AppRoutes';
-import AppRoutes from "./routes/AppRoutes"
+import AppRoutes from './routes/AppRoutes';
 import ErrorBoundary from './components/ErrorBoundary';
 import { BrowserRouter as Router } from 'react-router-dom';
 import '../node_modules/flag-icon-css/css/flag-icons.min.css';
